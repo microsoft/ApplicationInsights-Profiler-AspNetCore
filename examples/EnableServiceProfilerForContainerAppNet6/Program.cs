e72d16df1fd80fa36e2e9fb41f6c8257071bd613
Saar Shen <xiaomi7732@hotmail.com>
2022-03-28 17:05:21 -0700

    Add dockerize part

--- examples/EnableServiceProfilerForContainerAppNet6/Program.cs
+++ examples/EnableServiceProfilerForContainerAppNet6/Program.cs
@@ -2,7 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationInsightsTelemetry();
-builder.Services.AddServiceProfiler();
+builder.Services.AddServiceProfiler(profilerSettings =>
+{
+    profilerSettings.Duration = TimeSpan.FromMinutes(1);
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
