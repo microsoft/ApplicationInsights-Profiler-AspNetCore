1ae7298196f435f28bcdbb5e8efdd3c0258dac5f
Saar Shen <saars@microsoft.com>
2024-08-15 10:09:56 -0700

    Update .net 8 example

--- examples/EnableServiceProfilerNet8/Program.cs
+++ examples/EnableServiceProfilerNet8/Program.cs
@@ -1,16 +1,17 @@
+
+using HeavyArrayForecast;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationInsightsTelemetry(); // Register Application Insights
-builder.Services.AddServiceProfiler(opt =>{
+builder.Services.AddServiceProfiler(opt =>
+{
     // Customize by code, for example:
     opt.Duration = TimeSpan.FromSeconds(30);    // Profile for 30 seconds than the default of 2 minutes.
 });  // Register Profiler
 
 var app = builder.Build();
 
-app.MapGet("/", async () => {
-    await Task.Delay(500);
-    return "Hello Profiler";
-});
+app.MapGet("/", () => WeatherForecastHelper.GetForecasts());
 
 app.Run();
